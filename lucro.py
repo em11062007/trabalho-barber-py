e584ba8e0e439a9dbdfa88a2f1d77b23373f2e15
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-10-01 09:01:01 -0300

    Create lucro.py

---
+++ lucro.py
@@ -0,0 +1,8 @@
+print("faça seu cálculo do dia logo abaixo:")
+v1=int(input("coloque o valor do corte:"))
+v2=int(input("coloque a quantidade de cortes realizados:"))
+def soma(v1, v2):
+    return v1 * v2
+print("\n")
+resultado = soma(v1, v2)
+print(f"O lucro do dia é: {resultado}")
