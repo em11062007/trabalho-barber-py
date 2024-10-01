309dbcd2f07629c90a81033be706dc50567513e0
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-10-01 09:00:46 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,19 @@
+print("faça seu cadastro da pessoa 1 aqui abaixo:")
+sexo1=input("insira seu sexo:")
+idade1=int(input("coloque sua idade:"))
+nome1=input("coloque seu nome:")
+
+print("faça seu cadastro da pessoa 2 aqui abaixo:")
+sexo2=input("insira seu sexo:")
+idade2=int(input("coloque sua idade:"))
+nome3=input("coloque seu nome:")
+
+print("faça seu cadastro da pessoa 3 aqui abaixo:")
+sexo3=input("insira seu sexo:")
+idade3=int(input("coloque sua idade:"))
+nome3=input("coloque seu nome:")
+print("\n")
+
+print("cadastro1:", sexo1,idade1,nome1)
+print("cadastro2:", sexo1,idade2,nome3)
+print("cadastro3:", sexo3,idade3,nome3)
