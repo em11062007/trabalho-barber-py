8143c1e019a820368b3b48354e608171074030bf
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-10-22 09:35:02 -0300

    Update and rename cadastro.py to indez.html

--- cadastro.py
+++
@@ -1,8 +0,0 @@
-print("faça seu cadastro da pessoa 1 aqui abaixo:")
-sexo1=input("insira seu sexo:")
-idade1=int(input("coloque sua idade:"))
-nome1=input("coloque seu nome:")
-
-print("\n")
-
-print("cadastro1:", sexo1,idade1,nome1)
